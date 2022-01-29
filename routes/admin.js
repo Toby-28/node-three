50ee983754a54dbc64f108ba852aa6f9627216bf
Toby-28 <tobymarshal2802@gmail.com>
2022-01-29 15:23:58 +0500

    s

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const { getAddProduct, pushProduct } = require('../controllers/products')
+const { getAddProduct, pushProduct } = require('../controllers/admin')
 const router = express.Router()
 
 // /admin/add-product => GET
 router.get('/add-product', getAddProduct)
+
+// /admin/products => GET
+router.get('/products')
 
 // /admin/add-product => POST
 router.post('/add-product', pushProduct)
